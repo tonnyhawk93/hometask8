5765d4bf1e5bb9e33da978b28afda2ac1be23230
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-28 17:01:59 +0300

    add test main

--- test/hermione/layout.hermione.js
+++ test/hermione/layout.hermione.js
@@ -1,3 +1,5 @@
+const { delay } = require("rxjs");
+
 describe('Проверка вёрстки страниц', async function() {
     it('Проверка вёрстки главной страницы', async function() {
         const browser = this.browser;
@@ -33,6 +35,22 @@ describe('Проверка вёрстки страниц', async function() {
         const page = await browser.$('.Application');
         await page.waitForExist();
         await browser.assertView('emptyCartPage', '.Application', {
+            allowViewportOverflow: true
+        });
+    });
+    it('Можно сделать заказ', async function() {
+        const browser = this.browser;
+        await browser.url('/hw/store/catalog/0');
+        const button = await browser.$('.ProductDetails-AddToCart');
+        await button.waitForExist();
+        button.click();
+        await browser.$('.nav-link:nth-child(4)').click();
+        await browser.$('#f-name').setValue('test-name');
+        await browser.$('#f-phone').setValue('4235234523452345');
+        await browser.$('#f-address').setValue('sfgsfgsfgsfdsf');
+        await browser.$('.Form-Submit').click();
+        delay(5000)
+        await browser.assertView('orderPage', '.Application', {
             allowViewportOverflow: true
         });
     });
