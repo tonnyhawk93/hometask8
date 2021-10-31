9e10b2596ef3f40a19541f93026e3e2406175380
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-31 22:57:37 +0300

    main

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -3,4 +3,10 @@ echo  ${{steps.build_changelog.outputs.changelog}}
 echo  ${{steps.build_changelog.outputs.fromTag}}
 echo ${{steps.build_changelog.outputs.toTag}}
 
+# curl \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/octocat/hello-world/git/blobs \
+#   -d '{"content":"content"}'
+
 
