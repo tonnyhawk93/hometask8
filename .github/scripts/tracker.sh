530e01fc3347e92e6642a174b008b63832d7e176
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-31 23:23:37 +0300

    test 1

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-echo "Steps.build_changelog.outputs.changelog is ${{steps.build_changelog.outputs.changelog}}"
+echo "Steps.build_changelog.outputs.toTag is ${{steps.build_changelog.outputs.toTag}}"
 
 
