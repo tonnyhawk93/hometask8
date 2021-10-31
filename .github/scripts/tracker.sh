38bbff84ec1110b6baf14cf19910301eb065db4c
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-31 23:12:52 +0300

    steps.build_changelog.outputs.changelog

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -1,12 +1,5 @@
 #! /usr/bin/env bash
-echo  ${{steps.build_changelog.outputs.changelog}}
-echo  ${{steps.build_changelog.outputs.fromTag}}
-echo ${{steps.build_changelog.outputs.toTag}}
 
-# curl \
-#   -X POST \
-#   -H "Accept: application/vnd.github.v3+json" \
-#   https://api.github.com/repos/octocat/hello-world/git/blobs \
-#   -d '{"content":"content"}'
-#git update-index --chmod=+x ./.github/scripts/tracker.sh
+echo "Steps.build_changelog.outputs.changelog is ${{steps.build_changelog.outputs.changelog}}"
+
 
