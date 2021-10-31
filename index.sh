ab9915c9a07b5ee62f0c7f9485f2456320ff9e9b
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-31 16:32:53 +0300

    change sh

--- index.sh
+++ index.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
+npm i
 npm run build
-selenium-standalone start & $!> ./selenium
+selenium-standalone start & echo $! > ./selenium
 npm test
-pkill -F ./selenium && rm ./selenium
+pkill -F ./selenium
+rm ./selenium
 echo "done"
