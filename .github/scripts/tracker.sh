12f5c5a95ff46fe77dcb7eddcbf82af4bf3e27e0
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-11-03 14:45:45 +0300

    response id add

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -24,9 +24,9 @@ response=$(curl --silent -s 'https://api.tracker.yandex.net/v2/issues/' \
 --data-raw "$Data"
 )
 
-echo '$response'
-responseId = $('$response' | jq -r '.id')
-echo '$responseId'
+echo $response
+responseId = $("${response}"| jq -r '.id')
+echo $responseId
 if [ -n "$responseId"]
   then echo "OK!" 
   else echo "Error!" 
