8cf082d87dae37661efb22c1a56858a9fe1938fe
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-11-04 04:36:11 +0300

    add comment

--- .github/scripts/add.sh
+++ .github/scripts/add.sh
@@ -4,19 +4,15 @@ Id=$2
 OAuth=$3
 OrganizationId=$4
 Data='{
-        "text": '${Text}'
+        "text": "'"${Text}"'"
     }'
     
-responseCode=$(curl --silent  -o /dev/null -s -w "%{http_code}" --location --request POST "https://api.tracker.yandex.net/v2/issues/$Id/comments" \
+responseCode=$(curl --silent -s "https://api.tracker.yandex.net/v2/issues/$Id/comments" \
 --header "Authorization: OAuth $OAuth" \
 --header "X-Org-ID: $OrganizationId" \
---header "Content-Type: application/json" \
 --data-raw "$Data"
 )
-echo $Text
-echo "https://api.tracker.yandex.net/v2/issues/$Id/comments"
-echo $OAuth
-echo $OrganizationId
+echo $responseCode
 if [ "$responseCode" = 201 ]
 then echo "Comment created successfully!"
 else echo "Error!!! Comment not created."
