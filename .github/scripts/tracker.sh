a057f6cf0c8aa787808cbfebb13b5e87b638b8c6
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-11-06 18:43:16 +0300

    add refactor code in curl and add update tickets

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -16,27 +16,38 @@ Data='{
         "unique": "'"${Unique}"'"
     }'
 
-responseId=$(curl -s 'https://api.tracker.yandex.net/v2/issues/' \
--H "Authorization: OAuth $OAuth" \
--H "X-Org-ID: $OrganizationId" \
--H "Content-Type: application/json" \
--d "$Data" \
-| jq -r '.id')
 
+response=$(curl -s --write-out "%{http_code}" "https://api.tracker.yandex.net/v2/issues/_search" \
+    -H "Authorization: OAuth $OAuth" \
+    -H "X-Org-ID: $OrganizationId" \
+    -H "Content-Type: application/json" \
+    -d '{"filter": {"unique": "'"${Unique}"'"}}' 
+    )
 
-if [[ "$responseId" != "null" ]]
+if [[ $response -ne 201 ]]
   then 
-    echo "ticketId=$responseId" >> $GITHUB_ENV
-    echo "В трекере создан релизный тикет с ID = $responseId" 
-    exit 0
+    responseId=$(curl -s 'https://api.tracker.yandex.net/v2/issues/' \
+    -H "Authorization: OAuth $OAuth" \
+    -H "X-Org-ID: $OrganizationId" \
+    -H "Content-Type: application/json" \
+    -d "$Data" \
+    | jq -r '.id')
+    if [[ "$responseId" != "null" ]]
+      then
+        echo "ticketId=$responseId" >> $GITHUB_ENV
+        echo "В трекере создан релизный тикет с ID = $responseId" 
+        exit 0
+      else
+        echo "Ошиба создания тикета в трекере" 
+        exit 1
+    fi
   else 
     responseId=$(curl -s "https://api.tracker.yandex.net/v2/issues/_search" \
     -H "Authorization: OAuth $OAuth" \
     -H "X-Org-ID: $OrganizationId" \
     -H "Content-Type: application/json" \
-    -d '{"filter": {"unique": "'"${Unique}"'"}}' \
-    | jq -r '.id'
-    )
+    -d '{"filter": {"unique": "'"${Unique}"'"}}' /
+    | jq -r '.id')
     if [[ "$responseId" != "null" ]]
       then 
         echo "ticketId=$responseId" >> $GITHUB_ENV
