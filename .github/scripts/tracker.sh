0a340c0a7fa409148abdf59a8e608000832e816e
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-11-06 19:10:35 +0300

    add refactor code in curl and add update tickets

--- .github/scripts/tracker.sh
+++ .github/scripts/tracker.sh
@@ -57,7 +57,7 @@ if [[ "$response" == "[]" ]]
           -H "X-Org-ID: $OrganizationId" \
           -H "Content-Type: application/json" \
           -d "$Data" \
-          | jq -r '.id'
+          | jq -r '[0].id'
           )
         if [[ "$responseId" != "null" ]]
           then 
